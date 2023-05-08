539345a03455b8c8987cd7d185c5d6fcf07b5273
luca betti sorbelli <luca betti sorbelli>
2023-05-08 16:25:26 +0200

    create img

--- js/script.js
+++ js/script.js
@@ -10,12 +10,12 @@ createApp({
     },
 }).mount('#app')
 
-// const { createImg } = Vue
+const { createImg } = Vue
 
-// createImg({
-//     data() {
-//         return {
-//             imagine: 
-//         }
-//     },
-// }).mount('#img')
+createImg({
+    data() {
+        return {
+            imagine: "https://www.coches.com/fotos_historicas/bmw/M5-Nighthawk-F10-Australia-2015/bmw_m5-nighthawk-f10-australia-2015_r15.jpg"
+        }
+    },
+}).mount('#img')
